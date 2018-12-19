689aeb090d991e907d1cfcf344e93f70effa5083
Geoff Jukes <geoff@jukes.org>
2018-12-19 14:14:03 -0800

    Initial Commit

--- render-json.py
+++ render-json.py
@@ -1,8 +1,7 @@
 import yaml
 import json
 
-src = 'draft.yml'
-tgt = src.replace('.yml', '.json')
-
-with open(tgt, 'w') as t:
-    t.write(json.dumps(yaml.load(open(src)), indent=2))
+for src in ['draft.yaml', 'schema.yaml']:
+    tgt = src.replace('.yaml', '.json')
+    with open(tgt, 'w') as t:
+        t.write(json.dumps(yaml.load(open(src)), indent=2))
